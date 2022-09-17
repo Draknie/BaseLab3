d77b5d068cb0009106ff601771b0cc8ffd68460e
AbramovDD <AbramovDD@310D-9>
2022-09-17 14:03:59 +0700

    Добавьте файлы проекта.

---
+++ BaseLab3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BaseLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+                int N = int.Parse(Console.ReadLine());
+                bool flag = true;
+                for (int i = 2; i < N - 1; i++)
+                {
+                    if (N % 1 == 0) flag = false;
+                }
+                if (flag)
+                {
+                    Console.WriteLine("Prime number");
+                }
+                else
+                {
+                    Console.WriteLine("Compesite number");
+                }
+     
+         }  
+    }
+}
